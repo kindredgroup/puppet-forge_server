fa440882a4e419538d509302f9079333ff873be1
Simonas Rupšys <Simonas.Rupsys@adform.com>
2016-07-08 14:57:34 +0300

    use 'init' service provider

--- manifests/service.pp
+++ manifests/service.pp
@@ -8,6 +8,7 @@ class forge_server::service {
     service { 'puppet-forge-server':
       ensure   => $::forge_server::service_ensure,
       enable   => $::forge_server::service_enable,
+      provider => 'init',
     }
   }
 
