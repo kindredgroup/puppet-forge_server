ffbd54a40de6ba794fe10536199bed3cb5093f7a
Simonas Rupšys <Simonas.Rupsys@adform.com>
2016-07-08 14:57:15 +0300

    avoid resource duplication

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,9 +4,11 @@
 #
 class forge_server::service {
 
-  service { 'puppet-forge-server':
-    ensure => $::forge_server::service_ensure,
-    enable => $::forge_server::service_enable
+  if $::forge_server::service_enable == true {
+    service { 'puppet-forge-server':
+      ensure   => $::forge_server::service_ensure,
+      enable   => $::forge_server::service_enable,
+    }
   }
 
 }
