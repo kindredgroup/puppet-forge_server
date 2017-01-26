36c1c8984b28edd1d40c4af1dd45d5aa65c5f863
Sebastian Gumprich <Sebastian.Gumprich@t-systems.com>
2017-01-26 15:20:14 +0100

    systemd, proxy
    * adds a systemd unit file
    * adds proxy support to init script and unit-file
    
    the unit files 'ExecStart' is a bit hacky, because you cannot append
    variables like you do in the default-config and use it in unit files.
    See https://github.com/systemd/systemd/issues/1082 for more info.
    
    This also adds a proxy-option environment variable.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,6 +5,7 @@
 class forge_server::params {
   $package = 'puppet-forge-server'
   $user = 'forge'
+  $user_homedir = '/home/forge'
   $pidfile = '/var/run/puppet-forge-server/forge-server.pid'
   $port = 8080
   $bind_host = '127.0.0.1'
@@ -13,6 +14,7 @@ class forge_server::params {
   $cache_basedir = '/var/lib/puppet-forge-server/cache'
   $log_dir = '/var/log/puppet-forge-server'
   $provider = 'gem'
+  $http_proxy    = undef
   $scl_install_timeout = 300
   $scl_install_retries = 1
 }
