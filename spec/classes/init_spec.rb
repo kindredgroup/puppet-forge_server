6df18853c7a8755560aee6a3efca819298cf3060
Drew Jess <drew.jess@fanduel.com>
2017-03-17 15:44:42 +0000

    update tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,92 +1,150 @@
 require 'spec_helper'
 describe 'forge_server' do
 
-  context 'with defaults for all parameters' do
-    it { should compile.with_all_deps }
-    it {
-      should contain_package('puppet-forge-server')
-    }
-    it {
-      should_not contain_exec('scl_install_forge_server')
-    }
-    it {
-      should_not contain_file('/etc/init.d/puppet-forge-server').with_content(/export LD_LIBRARY_PATH=.*\nexport GEM_PATH=.*\nexport PATH=.*\n/)
-    }
+  on_supported_os.each do |os, facts|
+    context "with defaults for all parameters (on #{os})" do
+      let(:facts) do
+        facts
+      end
+
+      it { should compile.with_all_deps }
+      it {
+        should contain_package('puppet-forge-server')
+      }
+      it {
+        should_not contain_exec('scl_install_forge_server')
+      }
+      it {
+        should_not contain_file('/etc/init.d/puppet-forge-server').with_content(/export LD_LIBRARY_PATH=.*\nexport GEM_PATH=.*\nexport PATH=.*\n/)
+      }
+    end
+
+
+    context "with single module path and proxy (on #{os})" do
+      let(:params) { {
+        :module_directory => '/dir1',
+        :proxy            => 'proxy1'
+      } }
+
+      let(:facts) do
+        facts
+      end
+
+      it { should compile.with_all_deps }
+
+      case facts[:osfamily]
+      when 'RedHat'
+        it {
+          should contain_file('/etc/default/puppet-forge-server').with_content(/PARAMS="\${PARAMS} --module-dir \\"\/dir1\\""\n\n/)
+        }
+        it {
+          should contain_file('/etc/default/puppet-forge-server').with_content(/PARAMS="\${PARAMS} --proxy proxy1"\n\n/)
+        }
+      when 'Debian'
+        it {
+          should contain_file('/etc/default/puppet-forge-server').with_content(/PARAMS="\${PARAMS} --module-dir \/dir1"\n\n/)
+        }
+        it {
+          should contain_file('/etc/default/puppet-forge-server').with_content(/PARAMS="\${PARAMS} --proxy proxy1"\n\n/)
+        }
+       end
+    end
+
+
+    context "with multiple module paths and proxies (on #{os})" do
+      let(:params) { {
+        :module_directory => ['/dir1', '/dir2'],
+        :proxy            => ['proxy1', 'proxy2']
+      } }
+
+      let(:facts) do
+        facts
+      end
+
+      it { should compile.with_all_deps }
+
+      case facts[:osfamily]
+      when 'RedHat'
+        it {
+          should contain_file('/etc/default/puppet-forge-server').with_content(/PARAMS="\${PARAMS} --module-dir \\"\/dir1\\""\nPARAMS="\${PARAMS} --module-dir \\"\/dir2\\""\n/)
+        }
+        it {
+          should contain_file('/etc/default/puppet-forge-server').with_content(/PARAMS="\${PARAMS} --proxy proxy1"\nPARAMS="\${PARAMS} --proxy proxy2"\n/)
+        }
+      when 'Debian'
+        it {
+          should contain_file('/etc/default/puppet-forge-server').with_content(/PARAMS="\${PARAMS} --module-dir \/dir1"\nPARAMS="\${PARAMS} --module-dir \/dir2"\n/)
+        }
+        it {
+          should contain_file('/etc/default/puppet-forge-server').with_content(/PARAMS="\${PARAMS} --proxy proxy1"\nPARAMS="\${PARAMS} --proxy proxy2"\n/)
+        }
+      end
+    end
+
+
+    context "with service_refresh => false (on #{os})" do
+      let(:params) { {
+        :service_refresh  => false
+      } }
+
+      let(:facts) do
+        facts
+      end
+
+      it { should compile.with_all_deps }
+      it {
+        should_not contain_class('::forge_server::package').that_notifies('Class[::forge_server::service]')
+      }
+      it {
+        should_not contain_class('::forge_server::config').that_notifies('Class[::forge_server::service]')
+      }
+    end
+
+
+    context "with scl => ruby193 (on #{os})" do
+      let(:params) { {
+        :scl  => 'ruby193'
+      } }
+
+      let(:facts) do
+        facts
+      end
+
+      case facts[:osfamily]
+      when 'RedHat'
+        it { should compile.with_all_deps }
+        it {
+          should_not contain_package('puppet-forge-server')
+        }
+        it {
+          should contain_exec('scl_install_forge_server').with(
+            :command  => "scl enable ruby193 'gem install --bindir /usr/bin --no-rdoc --no-ri puppet-forge-server'"
+          )
+        }
+        it {
+          should contain_file('/etc/init.d/puppet-forge-server').with_content(/export LD_LIBRARY_PATH=.*\nexport GEM_PATH=.*\nexport PATH=.*\n/)
+        }
+      end
+    end
+
+
+    context "with scl => ruby193, scl_install_timeout => 60000, scl_install_retries => 10 (on #{os})" do
+      let(:params) { {
+        :scl                 => 'ruby193',
+        :scl_install_timeout => 60000,
+        :scl_install_retries => 10
+      } }
+
+      let(:facts) do
+        facts
+      end
+
+      case facts[:osfamily]
+      when 'RedHat'
+        it { should compile.with_all_deps }
+        it { should contain_exec('scl_install_forge_server').with_timeout(60000) }
+        it { should contain_exec('scl_install_forge_server').with_tries(10) }
+      end
+    end
   end
-
-  context 'with single module path and proxy' do
-    let(:params) { {
-      :module_directory => '/dir1',
-      :proxy            => 'proxy1'
-    } }
-
-    it { should compile.with_all_deps }
-    it {
-      should contain_file('/etc/default/puppet-forge-server').with_content(/PARAMS="\${PARAMS} --module-dir \\"\/dir1\\""\n\n/)
-    }
-    it {
-      should contain_file('/etc/default/puppet-forge-server').with_content(/PARAMS="\${PARAMS} --proxy proxy1"\n\n/)
-    }
-  end
-
-  context 'with multiple module paths and proxies' do
-    let(:params) { {
-      :module_directory => ['/dir1', '/dir2'],
-      :proxy            => ['proxy1', 'proxy2']
-    } }
-
-    it { should compile.with_all_deps }
-    it {
-      should contain_file('/etc/default/puppet-forge-server').with_content(/PARAMS="\${PARAMS} --module-dir \\"\/dir1\\""\nPARAMS="\${PARAMS} --module-dir \\"\/dir2\\""\n/)
-    }
-    it {
-      should contain_file('/etc/default/puppet-forge-server').with_content(/PARAMS="\${PARAMS} --proxy proxy1"\nPARAMS="\${PARAMS} --proxy proxy2"\n/)
-    }
-  end
-
-  context 'with service_refresh => false' do
-    let(:params) { {
-      :service_refresh  => false
-    } }
-
-    it { should compile.with_all_deps }
-    it {
-      should_not contain_class('::forge_server::package').that_notifies('Class[::forge_server::service]')
-    }
-    it {
-      should_not contain_class('::forge_server::config').that_notifies('Class[::forge_server::service]')
-    }
-  end
-
-  context 'with scl => ruby193' do
-    let(:params) { {
-      :scl  => 'ruby193'
-    } }
-
-    it { should compile.with_all_deps }
-    it {
-      should_not contain_package('puppet-forge-server')
-    }
-    it {
-      should contain_exec('scl_install_forge_server').with(
-        :command  => "scl enable ruby193 'gem install --bindir /usr/bin --no-rdoc --no-ri puppet-forge-server'"
-      )
-    }
-    it {
-      should contain_file('/etc/init.d/puppet-forge-server').with_content(/export LD_LIBRARY_PATH=.*\nexport GEM_PATH=.*\nexport PATH=.*\n/)
-    }
-  end
-
-  context 'with scl => ruby193, scl_install_timeout => 60000, scl_install_retries => 10' do
-    let(:params) { {
-      :scl                 => 'ruby193',
-      :scl_install_timeout => 60000,
-      :scl_install_retries => 10
-    } }
-
-    it { should compile.with_all_deps }
-    it { should contain_exec('scl_install_forge_server').with_timeout(60000) }
-    it { should contain_exec('scl_install_forge_server').with_tries(10) }
-  end
-
 end
