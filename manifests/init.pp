ea0d23949fdbe375313fbcf406431e7e08dceb2a
Ger Apeldoorn <info@gerapeldoorn.nl>
2017-07-05 12:50:10 +0200

    Made http_proxy parameter overridable

--- manifests/init.pp
+++ manifests/init.pp
@@ -40,6 +40,9 @@
 # [*module_directory*]
 #   Directory of modules to serve, can be an array of directories
 #
+# [*http_proxy*]
+#   Use proxyserver for http(s) connections
+#
 # [*proxy*]
 #   Proxy requests to this upstream forge url
 #
@@ -79,6 +82,7 @@ class forge_server (
   $bind_host           = $::forge_server::params::bind_host,
   $daemonize           = true,
   $module_directory    = $::forge_server::params::module_directory,
+  $http_proxy          = $::forge_server::params::http_proxy,
   $proxy               = $::forge_server::params::proxy,
   $cache_basedir       = $::forge_server::params::cache_basedir,
   $log_dir             = $::forge_server::params::log_dir,
