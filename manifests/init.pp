f6eb3d03a852e3c1016c3276f5b124ca8a817666
beilers <bernd.eilers@parshipgroup.com>
2020-08-05 13:57:08 +0200

    fix issue with forge_server_script, removed pifile parameter as it's not needed with systemd, remove scl reference from Debian systemd unit file template

--- manifests/init.pp
+++ manifests/init.pp
@@ -91,7 +91,7 @@ class forge_server (
   $log_dir             = $::forge_server::params::log_dir,
   $debug               = false,
   $provider            = 'gem',
-  $forge_server_script = 'puppet-forge-server.ruby2.1',
+  $forge_server_script = $::forge_server::params::forge_server_script,
 ) inherits forge_server::params {
 
   if $scl {
